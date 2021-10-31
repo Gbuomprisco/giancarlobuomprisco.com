182c098cf8f237727f7673279029f02800d7d2a0
giancarlo <giancarlopsk@gmail.com>
2021-10-31 13:55:10 +0100

    Updates

---
+++ _next/static/A6YjU0-11sZzJwvj1cpmK/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog","\u002Farticles","\u002F[collection]","\u002Fseries\u002F[series]","\u002Fhubs\u002F[hub]","\u002F[collection]\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
