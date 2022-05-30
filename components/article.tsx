cc994aa3127bee9a106fb70dc7004b194790195a
giancarlo <giancarlopsk@gmail.com>
2022-05-31 01:49:49 +0200

    Published article

--- components/article.tsx
+++ components/article.tsx
@@ -129,7 +129,11 @@ const Article: React.FC<{
                 <div className="mb-10 mt-4 max-w-2xl mx-auto flex flex-col space-y-4">
                   <PostHeader post={post} />
 
-                  <SeriesList posts={series} series={post.series} />
+                  {series ? (
+                    <div className={"my-8"}>
+                      <SeriesList posts={series} series={post.series} />
+                    </div>
+                  ) : null}
                 </div>
 
                 <PostBody content={content} />
