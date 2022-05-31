b0a098990b428c88dbd03f9da021bb6cf88628f9
giancarlo <giancarlopsk@gmail.com>
2022-05-31 13:39:44 +0200

    Fixed Cal Widget

--- components/cal-widget.tsx
+++ components/cal-widget.tsx
@@ -1,25 +1,53 @@
+import { useEffect, useRef } from "react";
+
+declare var Cal: any;
+
+declare global {
+  interface Window {
+    Cal: typeof Cal;
+  }
+}
 
 const CalWidget = () => {
+  const mounted = useRef(false);
+
+  useEffect(() => {
+    if (mounted.current) {
+      return;
+    }
+
+    loadCalScript();
+    initCalDotCom();
+
+    mounted.current = true;
+  }, []);
+
   return (
     <>
       <div className={'w-full h-full overflow-scroll'} id="my-cal-inline" />
-
-      <script dangerouslySetInnerHTML={{
-        __html: `(function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; typeof namespace === "string" ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar); return; } p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
-          Cal("init", {origin:"https://app.cal.com"});
-          Cal("inline", {
-          elementOrSelector:"#my-cal-inline",
-          calLink: "gianc/30min"
-        });
-          Cal("ui", {
-          theme: "light",
-          styles: {
-          branding: {"brandColor":"#fff"}
-        }
-        })`
-      }} />
     </>
   );
 };
+
+function initCalDotCom() {
+  Cal("init", {origin:"https://app.cal.com"});
+
+  Cal("inline", {
+    elementOrSelector:"#my-cal-inline",
+    calLink: "gianc/30min"
+  });
+
+  Cal("ui", {
+    theme: "light",
+    styles: {
+      branding: {"brandColor":"#fff"}
+    }
+  });
+}
+
+function loadCalScript() {
+  (function (C, A, L) { let p = function (a: { q: unknown[] }, ar: unknown[]) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { // @ts-ignore
+    p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; typeof namespace === "string" ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar); return; } p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
+}
 
 export default CalWidget;
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -52,7 +52,7 @@ const Navigation = () => {
       <li
         style={style}
         className={
-          "text-lg lg:text-sm text-gray-600 border-b-4 pb-1" +
+          "text-lg lg:text-sm text-gray-500 border-b-4 pb-1" +
           " border-b-transparent transition:border " +
           (active
             ? "font-bold  border-b-yellow-200"
@@ -96,7 +96,7 @@ const Navigation = () => {
       </div>
 
       <ul
-        className="lg:space-x-8 text-gray-800 px-4 hidden flex-col space-y-4 lg:space-y-0 lg:flex lg:flex-row nav z-20"
+        className="lg:space-x-4 text-gray-700 px-4 hidden flex-col space-y-4 lg:space-y-0 lg:flex lg:flex-row nav z-20"
         ref={nav}
       >
         <li className="flex flex-row justify-between mb-6 lg:hidden">
